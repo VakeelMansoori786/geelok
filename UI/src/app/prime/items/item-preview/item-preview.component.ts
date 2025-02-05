eaec93f0bb87e7e8f28206cb4077e6894034edc7
vakeelmansoori786 <vakeelmansoori786@gmail.com>
2025-02-05 08:11:55 +0400

    transaction

--- UI/src/app/prime/items/item-preview/item-preview.component.ts
+++ UI/src/app/prime/items/item-preview/item-preview.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LocalStoreService } from '../../services/local-store.service';
+import { ConfirmationService, MessageService } from 'primeng/api';
+import { APIService } from '../../services/api.service';
+import { AutoCompleteCompleteEvent } from 'primeng/autocomplete';
+import { forkJoin } from 'rxjs';
+
 
 @Component({
   selector: 'app-item-preview',
   templateUrl: './item-preview.component.html',
-  styleUrls: ['./item-preview.component.scss']
+  styleUrls: ['./item-preview.component.scss'],
+    providers: [MessageService,ConfirmationService]
 })
 export class ItemPreviewComponent {
-
+  constructor(
+    private formBuilder:FormBuilder,
+      private route: ActivatedRoute,
+      private router: Router,
+      private ls:LocalStoreService,
+      private apiService:APIService,
+      private service: MessageService
+      ) { }
 }
